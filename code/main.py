5ea4bfb2001befda0e3fa0dd237fc7882ed757f8
Ruben Lopez <24.lopezr@gmail.com>
2022-04-05 08:46:38 +0200

    Updated for only evaluation uses + minor changes in plots

--- code/main.py
+++ code/main.py
@@ -8,6 +8,7 @@ from keras.models import load_model
 from optparse import OptionParser
 from sklearn.preprocessing import StandardScaler
 import importlib
+import os
 
 from utils.dataloader import load, scale
 from utils.evaluating import evaluate
@@ -16,7 +17,8 @@ from utils.training import train
 from utils.plotting import print_plots
 
 if __name__ == "__main__":
-	
+	os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 	# _________________________________________________________________________________________
 	# Read user options
 	parser = OptionParser(usage="%prog --help")
@@ -27,11 +29,10 @@ if __name__ == "__main__":
 	parser.add_option("-l", "--latent",    dest="latent",      type="int",      default=64,               help="Dimension of latent space")
 	parser.add_option("-b", "--batch",     dest="batch",       type="int",      default=256,              help="N batch")
 	parser.add_option("-k", "--khyp",      dest="k_hyp",       type="int",      default=5,                help="k hyperparameter")
-	
 	parser.add_option("-m", "--init",      dest="init",        type="string",   default=None,        help="Initial generator model")
 	parser.add_option("-t", "--no-train",  dest="train",       action="store_false")
 	(options, args) = parser.parse_args()
-	
+
 	# GAN model
 	gan_m = options.gan_model
 	# Name of input file
@@ -46,65 +47,68 @@ if __name__ == "__main__":
 	nbatch = options.batch
 	# number or discriminator updates
 	n_discrim_updates = options.k_hyp
-	
+
 	# initial gen model
+	g_model = None
 	if options.init is not None:
 		g_model_name = options.init
 		g_model = load_model(g_model_name)
-	print('_'*30)
+	print('_'*90)
 	print('OPTIONS')
-	print('_'*30)
+	print('_'*90)
 	print(' GAN model: {:<50}'.format(options.gan_model))
 	print(' Training data file: {:<50}'.format(options.input))
 	print(' Output model name: {:<50}'.format(options.output))
-	print(' Training mode: {:<50}'.format(str(options.train)))
-	print('     Training epochs: {:<50}'.format(str(options.epochs)))
-	print('     Dimension of latent space: {:<50}'.format(str(options.latent)))
-	print('     Batch size: {:<50}'.format(str(options.batch)))
-	print('     k hyperparameter: {:<50}'.format(str(options.k_hyp)))
-	print(' Initial generator model: {:<50}'.format(options.init))
-	print('_'*30)
+	print(' Training mode: {:<50}'.format('False' if options.train is False else 'True'))
+	if options.train is not False:
+		print('     Training epochs: {:<50}'.format(str(options.epochs)))
+		print('     Dimension of latent space: {:<50}'.format(str(options.latent)))
+		print('     Batch size: {:<50}'.format(str(options.batch)))
+		print('     k hyperparameter: {:<50}'.format(str(options.k_hyp)))
+	if options.init is not None:
+		print(' Initial generator model: {:<50}'.format(options.init))
+	print('_'*90)
 	# _________________________________________________________________________________________
-	
+
 	# GPU memory usage configuration
 	config = tf.compat.v1.ConfigProto()
 	config.gpu_options.allow_growth = True
 	sess = tf.compat.v1.Session(config=config)
 	backend.set_session(sess)
 	# _________________________________________________________________________________________
-	
-	# Create the models
-	GAN = importlib.import_module('models.'+gan_m)
-	# create the discriminator
-	d_model = GAN.define_discriminator()
-	d_model.summary()
-	# create the generator
-	if options.init is None:
-		g_model = GAN.define_generator(latent_dim)
-	g_model.summary()
-	# create the gan
-	gan_model = GAN.define_gan(g_model, d_model)
-	gan_model.summary()
-	# _________________________________________________________________________________________
-	
+
 	# load and scale data
 	scaler = StandardScaler()
-	data = load(inputfile)
+	data = load(inputfile, measurements=False)
 	dataset, _, scaler = scale(data, scaler)
 	# _________________________________________________________________________________________
-	
-	# train model
+
+	# Create and train model
 	if options.train is not False:
+		GAN = importlib.import_module('models.'+gan_m)
+		# create the discriminator
+		d_model = GAN.define_discriminator()
+		d_model.summary()
+		# create the generator
+		if options.init is None:
+			g_model = GAN.define_generator(latent_dim)
+		g_model.summary()
+		# create the gan
+		gan_model = GAN.define_gan(g_model, d_model)
+		gan_model.summary()
 		g_model = train(g_model, d_model, gan_model, dataset, latent_dim, GAN.step_decay, n_discrim_updates, epochs, nbatch)
 	# _________________________________________________________________________________________
 	
 	# Evaluate the model
-	print('Scaler mean: {:<15}'.format(str(scaler.mean_)))
-	print('Scaler variance: {:<15}'.format(str(scaler.var_)))
-	real = data[:,4:]
-	fake = generate_evaluation_samples(g_model, dataset, latent_dim, scaler)
-	evaluate(real, fake)
-	print_plots(real, fake, output)
+	if g_model is not None:
+		fake = generate_evaluation_samples(g_model, dataset, latent_dim, scaler)
+		evaluate(data[:,4:], fake)
+	else:
+		fake = None
+		print("."*90)
+		print('Warning: There is no generator model to evaluate')
+		print("."*90)
+	print_plots(data, fake, output)
 	# _________________________________________________________________________________________
 	
 	#Generate a few
--- code/utils/plotting.py
+++ code/utils/plotting.py
@@ -1,5 +1,5 @@
 ##################################################################################################
-#### Plotting module                                                                        ####
+#### Plotting module                                                                          ####
 ##################################################################################################
 import ROOT
 import numpy as np
@@ -50,15 +50,18 @@ def plot_positions(var1, var2, var1g, var2g):
     plt.rcParams["figure.figsize"] = (14,7)
     plt.rcParams["figure.titlesize"], plt.rcParams["axes.titlesize"] = (20,20)
     plt.rcParams["axes.labelsize"] = 18
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, sharey=True)
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
 
     xrange = (-50,50)
-    ax1.hist(var1, bins=100, density = True, histtype = 'step', color = 'black', range = xrange, label = 'data x2');
-    ax1.hist(var1g, bins=100, density = True, histtype = 'step', color = 'red', range = xrange, label = 'generated x2');
-    ax2.hist(var2, bins=100, density = True, histtype = 'step', color = 'black', range = xrange, label = 'data y2');
-    ax2.hist(var2g, bins=100, density = True, histtype = 'step', color = 'red', range = xrange, label = 'generated y2');
-    ax1.set_xlabel('$x_1$')
-    ax2.set_xlabel('$y_1$')
+    b = 200
+    ax1.hist(var1, bins=b, density = True, histtype = 'step', color = 'black', range = xrange, label = 'data x2');
+    if var1g is not None:
+    	ax1.hist(var1g, bins=b, density = True, histtype = 'step', color = 'red', range = xrange, label = 'generated x2');
+    ax2.hist(var2, bins=b, density = True, histtype = 'step', color = 'black', range = xrange, label = 'data y2');
+    if var2g is not None:
+    	ax2.hist(var2g, bins=b, density = True, histtype = 'step', color = 'red', range = xrange, label = 'generated y2');
+    ax1.set_xlabel('$x_2$')
+    ax2.set_xlabel('$y_2$')
     ax1.legend()
     ax2.legend()
     fig.suptitle('Positions');
@@ -68,15 +71,18 @@ def plot_directions(var1, var2, var1g, var2g, scale):
     plt.rcParams["figure.figsize"] = (14,7)
     plt.rcParams["figure.titlesize"], plt.rcParams["axes.titlesize"] = (20,20)
     plt.rcParams["axes.labelsize"] = 18
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, sharey=True)
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
 
     xrange = (-1.5, 1.5)
-    ax1.hist(var1, bins=100, density = True, histtype = 'step', color = 'black', range = xrange, label = 'data vx2');
-    ax1.hist(var1g, bins=100, density = True, histtype = 'step', color = 'red', range = xrange, label = 'generated vx2');
-    ax2.hist(var2, bins=100, density = True, histtype = 'step', color = 'black', range = xrange, label = 'data vy2');
-    ax2.hist(var2g, bins=100, density = True, histtype = 'step', color = 'red', range = xrange, label = 'generated vy2');
-    ax1.set_xlabel('$v_{x_1}$')
-    ax2.set_xlabel('$v_{y_1}$')
+    b = 200
+    ax1.hist(var1, bins=b, density = True, histtype = 'step', color = 'black', range = xrange, label = 'data vx2');
+    if var1g is not None:
+    	ax1.hist(var1g, bins=b, density = True, histtype = 'step', color = 'red', range = xrange, label = 'generated vx2');
+    ax2.hist(var2, bins=b, density = True, histtype = 'step', color = 'black', range = xrange, label = 'data vy2');
+    if var2g is not None:
+    	ax2.hist(var2g, bins=b, density = True, histtype = 'step', color = 'red', range = xrange, label = 'generated vy2');
+    ax1.set_xlabel('$v_{x_2}$')
+    ax2.set_xlabel('$v_{y_2}$')
     ax1.set_yscale(scale)
     ax1.legend()
     ax2.legend()
@@ -85,14 +91,18 @@ def plot_directions(var1, var2, var1g, var2g, scale):
 
 def plot_difference(var1, var2, var1g, var2g, x_range, labels, scale):
     plt.rcParams["figure.figsize"] = (14,7)
+    plt.rcParams["figure.titlesize"], plt.rcParams["axes.titlesize"] = (20,20)
+    plt.rcParams["axes.labelsize"] = 18
     xrange = (-5,5)
 
     fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
-    b = 150
-    h1 = ax1.hist(var1,  bins=b, density = False, range = x_range, histtype = 'step', color = 'black', label='data '+labels[0])
-    h1 = ax1.hist(var1g, bins=b, density = False, range = x_range, histtype = 'step', color = 'red',   label='generated '+labels[0])
-    h2 = ax2.hist(var2,  bins=b, density = False, range = x_range, histtype = 'step', color = 'black', label='data '+labels[1])
-    h2 = ax2.hist(var2g, bins=b, density = False, range = x_range, histtype = 'step', color = 'red',   label='generated '+labels[1])
+    b = 200
+    ax1.hist(var1,  bins=b, density = False, range = x_range, histtype = 'step', color = 'black', label='data '+labels[0]);
+    if var1g is not None:
+    	ax1.hist(var1g, bins=b, density = False, range = x_range, histtype = 'step', color = 'red',   label='generated '+labels[0]);
+    ax2.hist(var2,  bins=b, density = False, range = x_range, histtype = 'step', color = 'black', label='data '+labels[1]);
+    if var2g is not None:
+    	ax2.hist(var2g, bins=b, density = False, range = x_range, histtype = 'step', color = 'red',   label='generated '+labels[1]);
     ax1.set_xlabel(labels[0])
     ax2.set_xlabel(labels[1])
     ax1.set_yscale(scale)
@@ -103,26 +113,67 @@ def plot_difference(var1, var2, var1g, var2g, x_range, labels, scale):
 
 def plot_correlation_2Dhist(var1, var2, var1g, var2g, ranges, labels):
     plt.rcParams["figure.figsize"] = (14,7)
+    plt.rcParams["figure.titlesize"], plt.rcParams["axes.titlesize"] = (20,20)
+    plt.rcParams["axes.labelsize"] = 18
 
     fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
 
-    ax1.hist2d(var1, var2, bins=100, range = ranges, density = True,
+    ax1.hist2d(var1, var2, bins=200, range = ranges, density = True,
               norm=matplotlib.colors.LogNorm())
     ax1.set_xlabel(labels[0])
     ax1.set_ylabel(labels[1])
-    ax2.hist2d(var1g, var2g, bins=100, range = ranges, density = True,
-              norm=matplotlib.colors.LogNorm())
-    ax2.set_xlabel(labels[0])
-    ax2.set_ylabel(labels[1])
+    if var1g is not None:
+	    ax2.hist2d(var1g, var2g, bins=200, range = ranges, density = True,
+		      norm=matplotlib.colors.LogNorm())
+	    ax2.set_xlabel(labels[0])
+	    ax2.set_ylabel(labels[1])
     fig.suptitle('Correlation '+labels[0]+'-'+labels[1]);
     return fig
 
 
-def print_plots(real, fake, output):
+def print_plots(data, fake, output):
 	
-	out = PdfPages('evaluation_'+output+'.pdf')
-	out.savefig(plot_difference(real[:,0], real[:,1], fake[:,0], fake[:,1], (-50,50), ['$\Delta x$','$\Delta y$'], 'log'));
-	out.savefig(plot_difference(real[:,2], real[:,3], fake[:,2], fake[:,3], (-1.5,1.5), ['$\Delta v_x$','$\Delta v_y$'], 'log'));
-	out.savefig(plot_correlation_2Dhist(real[:,0], real[:,2], fake[:,0], fake[:,2], [[-15,15],[-1,1]], ['$\Delta x$','$\Delta v_x$']));
-	out.savefig(plot_correlation_2Dhist(real[:,1], real[:,3], fake[:,1], fake[:,3], [[-15,15],[-1,1]], ['$\Delta y$','$\Delta v_y$']));
-	out.close()
+	if fake is not None:
+		out = PdfPages('evaluation_'+output+'.pdf')
+		out.savefig(plot_difference(data[:,4], data[:,5], fake[:,0], fake[:,1], (-25,25), ['$\Delta x$','$\Delta y$'], 'linear'));
+		out.savefig(plot_difference(data[:,6], data[:,7], fake[:,2], fake[:,3], (-1.5,1.5), ['$\Delta v_x$','$\Delta v_y$'], 'log'));
+		out.savefig(plot_correlation_2Dhist(data[:,4], data[:,6], fake[:,0], fake[:,2], [[-15,15],[-1,1]], ['$\Delta x$','$\Delta v_x$']));
+		out.savefig(plot_correlation_2Dhist(data[:,5], data[:,7], fake[:,1], fake[:,3], [[-15,15],[-1,1]], ['$\Delta y$','$\Delta v_y$']));
+		
+		# Plot variables x2, y2, vx2, vy2
+		l = -39*2
+		x1 = data[:,0]
+		y1 = data[:,1]
+		vx1 = data[:,2]
+		vy1 = data[:,3]
+		x2_real = data[:,4] + x1 - l*vx1
+		y2_real = data[:,5] + y1 - l*vy1
+		vx2_real = data[:,6] + vx1
+		vy2_real = data[:,7] + vy1
+		x2_fake = fake[:,0] + x1 - l*vx1
+		y2_fake = fake[:,1] + y1 - l*vy1
+		vx2_fake = fake[:,2] + vx1
+		vy2_fake = fake[:,3] + vy1
+		out.savefig(plot_positions(x2_real, y2_real, x2_fake, y2_fake))
+		out.savefig(plot_directions(vx2_real, vy2_real, vx2_fake, vy2_fake, 'log'))
+		out.close()
+	else:
+		out = PdfPages('evaluation_'+output+'.pdf')
+		out.savefig(plot_difference(data[:,4], data[:,5], None, None, (-25,25), ['$\Delta x$','$\Delta y$'], 'log'));
+		out.savefig(plot_difference(data[:,6], data[:,7], None, None, (-1.5,1.5), ['$\Delta v_x$','$\Delta v_y$'], 'log'));
+		out.savefig(plot_correlation_2Dhist(data[:,4], data[:,6], None, None, [[-15,15],[-1,1]], ['$\Delta x$','$\Delta v_x$']));
+		out.savefig(plot_correlation_2Dhist(data[:,5], data[:,7], None, None, [[-15,15],[-1,1]], ['$\Delta y$','$\Delta v_y$']));
+		
+		# Plot variables x2, y2, vx2, vy2
+		l = -39*2
+		x1 = data[:,0]
+		y1 = data[:,1]
+		vx1 = data[:,2]
+		vy1 = data[:,3]
+		x2_real = data[:,4] + x1 - l*vx1
+		y2_real = data[:,5] + y1 - l*vy1
+		vx2_real = data[:,6] + vx1
+		vy2_real = data[:,7] + vy1
+		out.savefig(plot_positions(x2_real, y2_real, None, None))
+		out.savefig(plot_directions(vx2_real, vy2_real, None, None, 'log'))
+		out.close()
